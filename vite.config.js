bfbe44ecf22c94b9eccd66b6d6a21524bba7acb5
Ithan Moreira <ithan.moreira@gmail.com>
2025-07-16 16:25:15 -0400

    Update vite.config.js
    base:"/",
    	base:"/MenteModular/",

--- vite.config.js
+++ vite.config.js
@@ -186,7 +186,7 @@ logger.error = (msg, options) => {
 export default defineConfig({
 	customLogger: logger,
 	plugins: [react(), addTransformIndexHtml],
-	base:"/",
+	base:"/MenteModular/",
 	server: {
 		cors: true,
 		headers: {
